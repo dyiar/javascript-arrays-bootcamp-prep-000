a3646800ce40aa1927d299ceb16f88c355656561
Austin Blake <ummwhere@hotmail.com>
2017-11-29 08:39:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,8 +29,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
-  return array;
+  var e = array.slice(1);
+  return e;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
